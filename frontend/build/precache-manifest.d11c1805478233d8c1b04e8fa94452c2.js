b8bf2e594afb4745c3bfec419b68fa063b11f097
Michelle Quinn <42622727+shellbellquinn@users.noreply.github.com>
2021-01-21 19:53:16 -0600

    build

---
+++ frontend/build/precache-manifest.d11c1805478233d8c1b04e8fa94452c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a331a88dc1f5e7a5234eae0a6c2e57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2835ab16058ab117446e",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "5bcd7d05e7ba68681ac2",
+    "url": "/static/css/main.456d07bc.chunk.css"
+  },
+  {
+    "revision": "2835ab16058ab117446e",
+    "url": "/static/js/2.840f385c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.840f385c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcd7d05e7ba68681ac2",
+    "url": "/static/js/main.554b0931.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
