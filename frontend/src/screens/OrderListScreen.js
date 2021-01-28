e8ee361df0357d8bafab7cf3d991efb7d8db8838
Michelle Quinn <michelle.n.quinn@gmail.com>
2021-01-27 21:51:15 -0600

    orderlist bug

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -50,7 +50,7 @@ export default function OrderListScreen(props) {
             {orders.map((order) => (
               <tr key={order._id}>
                 <td>{order._id}</td>
-                <td>{order.user.name || "cannot find user"}</td>
+                {/* <td>{order.user.name || "cannot find user"}</td> */}
                 <td>{order.createdAt.substring(0, 10)}</td>
                 <td>{order.totalPrice.toFixed(2)}</td>
                 <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
